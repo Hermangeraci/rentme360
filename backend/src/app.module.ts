0a25f85c08c371de754e963a495342774b61510a
Hermangeraci <hermangeraci@gmail.com>
2025-11-19 16:05:52 +0100

    Add customer module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { VehicleService } from './vehicle.service';
 import { VehicleController } from './vehicle.controller';
+import { CustomerService } from './customer.service';
+import { CustomerController } from './customer.controller';
 
 @Module({
   imports: [],
-  controllers: [VehicleController],
-  providers: [PrismaService, VehicleService],
+  controllers: [VehicleController, CustomerController],
+  providers: [PrismaService, VehicleService, CustomerService],
 })
 export class AppModule {}
