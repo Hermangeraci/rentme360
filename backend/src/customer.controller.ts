528cf4e96bc558f6a95db934cb72a14aa633307c
Hermangeraci <hermangeraci@gmail.com>
2025-11-19 16:04:40 +0100

    customer.controller.ts

---
+++ backend/src/customer.controller.ts
@@ -0,0 +1,13 @@
+// backend/src/customer.controller.ts
+import { Controller, Get } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+
+@Controller('customers')
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Get()
+  findAll() {
+    return this.customerService.findAll();
+  }
+}
