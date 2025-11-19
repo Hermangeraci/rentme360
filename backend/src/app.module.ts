67ca1876325d83d46bd64bacaea00345537a60c9
Hermangeraci <hermangeraci@gmail.com>
2025-11-19 15:53:30 +0100

    Wire up Vehicle module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,1 +1,12 @@
-export class AppModule{}
+// backend/src/app.module.ts
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { VehicleService } from './vehicle.service';
+import { VehicleController } from './vehicle.controller';
+
+@Module({
+  imports: [],
+  controllers: [VehicleController],
+  providers: [PrismaService, VehicleService],
+})
+export class AppModule {}
